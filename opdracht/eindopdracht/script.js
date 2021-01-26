a61941906420855d1c9299957623c2516203a0ed
Ritchychica6 <2129361@talnet.nl>
2021-01-26 15:01:20 +0100

    fix

---
+++ opdracht/eindopdracht/script.js
@@ -0,0 +1,40 @@
+document.querySelector(js)
+
+var bezoeker=prompt(naam)
+var bezoeker=prompt(leeftijd)
+var bezoeker=prompt(achternaam)
+var bezoeker=prompt(email)
+var bezoeker=prompt(woonplaats)
+
+console.log(welkom op het internet)
+
+if(leeftijd <16) {
+    background-color="orange";
+}
+
+if(leeftijd <17) {
+    background-color="blue";
+
+}
+
+if(leeftijd <18) {
+    background-color="gold"
+
+}
+
+if(leeftijd <19-color){
+    background-color="green"
+
+    
+
+
+function myfuction bezoeker(naam, achternaam, leeftijd, email, woonplaats) {
+    this.naam=naam;
+    this.achternaam=achternaam;
+    this.leeftijd=leeftijd;
+    this.email=email;
+    this.woonplaats=woonplaats;
+
+
+
+}
