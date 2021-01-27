bcee207692241b0a8ea2963779274a5f0c02c53c
Ritchychica6 <2129361@talnet.nl>
2021-01-27 12:46:30 +0100

    klaar

--- opdracht/eindopdracht/script.js
+++ opdracht/eindopdracht/script.js
@@ -11,12 +11,21 @@
 
 //ik moet hier de voornaam laten zien die de gebruiker heeft ingetypt, dit doe ik met persoon.voornaam 
 document.getElementById("voornaam").innerHTML=persoon.voornaam;
-document.getElementById("achternaam").innerHTML=persoon.achternaam;
+document.getElementById("achternaam").innerHTML=persoon.achternaam; 
+document.getElementById("email").innerHTML=persoon.email;
+document.getElementById("woonplaats").innerHTML=persoon.woonplaats;
 
 if(persoon.leeftijd==16){
 // om de achtergrond van de body te veranderen gebruik ik in java script .style.backgrondColor=kleur
 document.getElementById("achtergrond").style.backgroundColor="orange";
 }
-else if(persoon.leeftijd==17){
-document.getElementById("achtergrond").style.backgroundColor="blue";
-}
+
+    else if(persoon.leeftijd==17){
+    document.getElementById("achtergrond").style.backgroundColor="blue";
+    }
+    else if(persoon.leeftijd==18){
+        document.getElementById("achtergrond").style.backgroundColor="gold";
+        }
+    else if(persoon.leeftijd==19){
+            document.getElementById("achtergrond").style.backgroundColor="green";
+            }
